9ae100811720ee5c960d87b114d793fdf706dc02
Topchik4 <47871459+Topchik4@users.noreply.github.com>
2019-03-10 19:44:02 +0200

    Update VideoHardware.py

--- lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
+++ lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
@@ -1,4 +1,4 @@
-rom Components.config import config, ConfigSelection, ConfigSubDict, ConfigYesNo
+from Components.config import config, ConfigSelection, ConfigSubDict, ConfigYesNo
 from Components.SystemInfo import SystemInfo
 from Tools.CList import CList
 from Tools.HardwareInfo import HardwareInfo
